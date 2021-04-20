23224c1de34ddcc35c964ee20591700ccb05f292
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-04-20 17:21:24 -0400

    Home page complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Typed from "react-typed"
+import {Link} from "react-router-dom"
 
 function Home () {
 
@@ -15,11 +16,14 @@ function Home () {
                 </div>
             <Typed
                 className="typed-text"
-                strings={["Full-Stack Software Engineer", "React"]}
+                strings={["Full-Stack Software Engineer", "Based in NYC", "JavaScript", "React", "Redux", "Ruby on Rails", "HTML", "CSS"]}
                 typeSpeed={40}
                 backSpeed={60}
                 loop
             />
+            <Link to="/contact" className="btn-main-offer">
+                Contact Me
+            </Link>
             
             </div>
         </div>
