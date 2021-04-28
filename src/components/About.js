239a41c7b59afe55a29f4497f6ea2c82e6bf02ff
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-04-28 15:45:11 -0400

    CSS changes

--- src/components/About.js
+++ src/components/About.js
@@ -17,14 +17,34 @@ function About () {
                         Hello! I'm Jeffrey. Most people call me Jeff. I'm a native New Yorker still based in the city.
                     </p>
                     <p className="about mb-3">
-                        After getting my Bachelors in Business Administration, I worked in sales and client management for seven years before finding my passion for coding and enrolling at Flatiron School.
+                        After getting my Bachelors in Business Administration, I worked in sales and client management for seven years before discovering my knack for coding.
                     </p>
                     <p className="about mb-3">
-                        I code with design in mind and am experienced in JavaScript-based programming, especially React and Redux, as well as Ruby on Rails. I get energized by a challenge and am always learning new skills to grow my tech stack. I love building new projects and one day hope to connect people across the world through technology.
+                    I enjoy creating things that live on the internet and code with design in mind. I get energized by a challenge and am always learning new languages and frameworks. One day, I hope to connect people across the world through accessible and inclusive technology.
                     </p>
-                    <p className="about mb-0">
+                    <p className="about mb-3">
                         I’m a street food connoisseur, I collect vinyls when I travel, I have a ridiculous amount of plants, and I’m a proud dog dad of a rescue from Thailand. 
                     </p>
+                    <p className="about mb-1">
+                        Technologies I've been working with recently:
+                    </p>
+                    <table class="table mb-0">
+                        <tbody>
+                            <tr>
+                                <td><span className="skill-arrow">ᐅ</span> JavaScript (ES6+)</td>
+                                <td><span className="skill-arrow">ᐅ</span> React</td>
+                            </tr>
+                            <tr>
+                                <td><span className="skill-arrow">ᐅ</span> Redux</td>
+                                <td><span className="skill-arrow">ᐅ</span> Ruby on Rails</td>
+                            </tr>
+                            <tr>
+                                <td><span className="skill-arrow">ᐅ</span> HTML</td>
+                                <td><span className="skill-arrow">ᐅ</span> CSS</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
                 </div>
             </div>
         </div>
