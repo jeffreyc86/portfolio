395363bd3ef42e25929b2e937b048f67a30a2702
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-04-29 18:28:21 -0400

    Navbar styling complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar () {
 
     
     return (
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
                 <span></span>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <FontAwesomeIcon icon={faBars} style={{color: "black"}}/>
