4340b1c85b60a1638f74c10ab6fccbde86b00c0a
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-04-29 11:25:24 -0400

    footer dimensions complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,27 +11,27 @@ function Footer() {
             <div className="container">
                 <div className="row">
                     <div className="col footer-col">
-                        <div className="d-flex">
+                        <div className="d-flex footer-flex">
                             <a className="footer-contact" href="tel:347-589-4735"><FontAwesomeIcon className="footer-contact-icon" icon={faMobileAlt} />+1.347.589.4735</a>
                         </div>
-                        <div className="d-flex">
+                        <div className="d-flex footer-flex">
                             <a className="footer-contact" href="mailto:mr.jeffrey.chiu@gmail.com"><FontAwesomeIcon className="footer-contact-icon" icon={faEnvelope} />mr.jeffrey.chiu@gmail.com</a>
                         </div>
                         
                     </div>
-                    <div className="col footer-col">
+                    <div className="col footer-col my-auto">
                         <div className="d-flex justify-content-center">
                             <a className="footer-icon" href="https://www.linkedin.com/in/jchiu86" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
                             <a className="footer-icon" href="https://github.com/jeffreyc86" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
                             <a className="footer-icon" href="https://jeffrey-chiu.medium.com" target="_blank"><FontAwesomeIcon icon={faMedium} /></a>
                         </div>
-                        <div className="copyright">
-                            <p className="pt-1 text-center">COPYRIGHT &copy; {new Date().getFullYear()} JEFFREY CHIU | ALL RIGHTS RESERVED</p>
-                        </div>
                     </div>
                    
                 </div>
             </div>
+                        <div className="copyright">
+                            <p className="text-center">COPYRIGHT &copy; {new Date().getFullYear()} JEFFREY CHIU | ALL RIGHTS RESERVED</p>
+                        </div>
             
         </div>
     )
