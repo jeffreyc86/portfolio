cd572ba6c3465a49fe55e7c1840512d0a770322b
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-05-04 14:06:03 -0400

    final version with timeline exp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ function Navbar () {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
-                        <li className="nav-item active">
+                        <li className="nav-item">
                             <Link smooth={true} to="home" className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
                         </li>
                         <li className="nav-item">
