655c1d3e5503d0aa7d8881521703daacc9fb15e4
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-06-15 12:33:52 -0400

    removed loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import Loader from "./components/Loader";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -8,22 +7,9 @@ import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import ReactGA from "react-ga";
-import { useSpring, animated } from "react-spring";
+
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
-  const props = useSpring({
-    to: { opacity: 0 },
-    from: { opacity: 1 },
-    delay: 1900,
-  });
-
-  useEffect(() => {
-    let timer = setTimeout(() => setLoaded(true), 2350);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [loaded]);
 
   // Google Analytics Set Up
   const gaTrackingId = "UA-197837085-1";
@@ -32,14 +18,6 @@ function App() {
 
   return (
     <div className="app">
-      {!loaded ? (
-        <div className="loader">
-          <animated.div style={props}>
-            <Loader />
-          </animated.div>
-        </div>
-      ) : (
-        <>
           <Navbar />
           <Home />
           <About />
@@ -47,8 +25,6 @@ function App() {
           <Experience />
           <Contact />
           <Footer />
-        </>
-      )}
     </div>
   );
 }
