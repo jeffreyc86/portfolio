6a7390c79323339c93cbcef800186b2cb1cb24ae
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2021-06-15 12:32:04 -0400

    loader added

---
+++ src/components/Loader.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { GooeyCircleLoader } from "react-loaders-kit";
+import Typed from "react-typed";
+
+function Loader() {
+  const loaderProps = {
+    loading: true,
+    size: 275,
+    duration: 2,
+    colors: ["#99fffe", "#f42e00", "#042549"],
+  };
+
+  return (
+    <div className="loader">
+      <div className="loader-content">
+        <GooeyCircleLoader {...loaderProps} />
+        <Typed
+          className="loader-text"
+          strings={["Loading..."]}
+          typeSpeed={60}
+          backSpeed={0}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import App from './App';
-import './fonts/SF-Pro.woff2'
-import './fonts/SF-Pro-Heavy.woff2'
+
 
 
 ReactDOM.render(
